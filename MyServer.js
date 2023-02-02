732b58887c8ae35bba7bbdb88abc9e8fc42048b3
Pavlo <ano3erone@gmail.com>
2023-02-02 17:34:53 +0200

    refactor: split one function into 4
    meaningful splits, don't see further possible splits

--- MyServer.js
+++ MyServer.js
@@ -1,20 +1,22 @@
+//setup modules
 const axios = require('axios');
 const cheerio = require('cheerio');
+
 const I_MIN_LIKES = 10;
 
-const getPostTitles = async () => {
-	try {
-		const { data } = await axios.get(
-			'https://stackoverflow.com/questions/17162334/how-to-use-continue-in-jquery-each-loop'
-            );
-		const $ = cheerio.load(data);
+//string f(doc?)
+const getTitle = ($)=>{
+    return $('#question-header  a.question-hyperlink').text();
+}
+
+//string f(doc?)
+const getQText = ($)=>{
+            $('div.postcell > div.js-post-body').text();
+}
 
 
-		const title = $('#question-header  a.question-hyperlink').text();
-
-		const qText = $('div.postcell > div.js-post-body').text();
-
-        let s_whole_text = title + qText;
+//string f(doc,string)
+const getAText = ($,header)=>{
 
 		$('div.answercell > div.js-post-body').each((_idx, el) => {
 		    const a_likes = $(el).closest('.post-layout').find('.js-vote-count').text();
@@ -23,8 +25,24 @@ const getPostTitles = async () => {
             if(parseInt(a_likes) < I_MIN_LIKES)return;
             console.log("str: " + a_likes);
             const answer ="comment:\n" +  $(el).text().replace("\n","\t")+"\n";
-			s_whole_text+=answer;
+			header+=answer;
 		});
+    return header;
+}
+const getPostTitles = async () => {
+	try {
+		const { data } = await axios.get(
+			'https://stackoverflow.com/questions/17162334/how-to-use-continue-in-jquery-each-loop'
+            );
+		const $ = cheerio.load(data);
+
+
+		const title = getTitle($);
+		const qText = getQText($);
+
+        let s_whole_text = title + qText;
+        s_whole_text = getAText($,s_whole_text);
+
 		return s_whole_text;
 	} catch (error) {
 	    console.error(e, e.stack);
