33579316876ddbbfc6e8e7e3dba7a22c40932314
Pavlo <ano3erone@gmail.com>
2023-02-03 17:06:44 +0200

    refactor: indented js files
    gg=G!

--- MyServer.js
+++ MyServer.js
@@ -30,67 +30,67 @@ const getTitle = ($)=>{
 
 //string f(doc?)
 const getQText = ($)=>{
-            return $('#question > div.post-layout > div.postcell.post-layout--right > div.s-prose.js-post-body');
+    return $('#question > div.post-layout > div.postcell.post-layout--right > div.s-prose.js-post-body');
 }
 
 
 //void f(jQuery_doc,&my_doc)
 const getAText = ($,doc)=>{
 
-		$('div.answercell > div.js-post-body').each((_idx, el) => {
-		    const a_likes = $(el).closest('.post-layout').find('.js-vote-count').text();
+    $('div.answercell > div.js-post-body').each((_idx, el) => {
+        const a_likes = $(el).closest('.post-layout').find('.js-vote-count').text();
 
-            const likes_on_A = parseInt(a_likes);
-            if( likes_on_A < I_MIN_LIKES){
-                return true;
-            }
+        const likes_on_A = parseInt(a_likes);
+        if( likes_on_A < I_MIN_LIKES){
+            return true;
+        }
 
         //    doc.html_text+='<p>this has helped: '+likes_on_A + '</p>';
- //is_answered to skip writing htmls w/o answers
-            doc.is_answered = true;
+        //is_answered to skip writing htmls w/o answers
+        doc.is_answered = true;
 
 
-			doc.html_text+=$(el);
-        });
+        doc.html_text+=$(el);
+    });
 }
 
 // string f(int)
 const getSOText = async (i_link) => {
-let doc = {
-    html_text:"",
-    is_answered:false
-};
-	try {
+    let doc = {
+        html_text:"",
+        is_answered:false
+    };
+    try {
         const { data } = await axios.get(
-			'https://stackoverflow.com/questions/' + i_link  + '/how-to-use-continue-in-jquery-each-loop'
+            'https://stackoverflow.com/questions/' + i_link  + '/how-to-use-continue-in-jquery-each-loop'
             ,httpAgent,httpsAgent).catch((error)=>{
                 console.log(error)
                 return null;
             });
         if (data === null) return;
-		const $ = cheerio.load(data);
+        const $ = cheerio.load(data);
 
 
-		const title = getTitle($);
-		const qText = getQText($);
+        const title = getTitle($);
+        const qText = getQText($);
         console.log(qText);
         doc.html_text = title + qText;
 
         getAText($,doc);
 
         if(doc.is_answered)
-		    return doc.html_text;
+            return doc.html_text;
         return null;
-	} catch (error) {
+    } catch (error) {
         console.error(error, error.stack);
-	    return null;
+        return null;
     }
 };
 
 const wrapped = limiter.wrap(getSOText);
 
 function mainLoop(){
-//{ file: '16992452.html', mtime: 2023-02-03T13:18:30.372Z }
+    //{ file: '16992452.html', mtime: 2023-02-03T13:18:30.372Z }
 
     const curr_num = getRecentLinkNum('./bin2');
     console.log(curr_num);
@@ -98,23 +98,23 @@ function mainLoop(){
     // push a promise to an array
     // promise_arr.push(wrapped(i));
     for( let i = curr_num ; i < curr_num; i--)
-//the number of crawls is too big rn
+        //the number of crawls is too big rn
         wrapped(i)
             .then((postTitles) => {
                 if(postTitles){
-                console.log(postTitles)
+                    console.log(postTitles)
 
-                fs.writeFile("./bin2/"+i+".html",postTitles,(error)=>{
-                if(error)
-                    return console.log(error);
-                console.log("file saved");
+                    fs.writeFile("./bin2/"+i+".html",postTitles,(error)=>{
+                        if(error)
+                            return console.log(error);
+                        console.log("file saved");
+                    });
+                }
             });
-        }
-    });
     //after all the crawling to change ip adress
     //const ip_promise = Promise.all(promise_arr);
     //ip_promise.then(changeMyIP());
-//void changeMyIP(void);
+    //void changeMyIP(void);
     //const changeMyIP = require('./src/windscribe_proxy.js');
     //there are probably some nodejs tools for proxying
     //also, maybe shouldn't even use async for a crawler at all, as later
