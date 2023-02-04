fbf705632e0ed814494a9821725dbfdb994e3b5a
Pavlo <ano3erone@gmail.com>
2023-02-04 16:16:01 +0200

    refactor: split parse into 3 functions

--- src/html_processor.js
+++ src/html_processor.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 
 //create a function which moves scraped html file into tmp folder, then parses it, returns multiple htmls, writes them into folder with the name corresponding to the scraped html's name
+
 //void f(string,string)
 async function move(path_pre,path_post){
     fs.rename(path_pre,path_post,(err)=>{
@@ -15,14 +16,32 @@ async function move(path_pre,path_post){
 
 const html_from_dir = "./bin/scraped_html/";
 const html_to_dir = "./bin/processed_html/";
+
 //string[] f(void)
 const getHtmlArr = (async ()=>{
     return fs.readdirSync(html_from_dir);
 });
+
 //void f(void)
 const parseAll = (async ()=>{
     const html_filenames = await getHtmlArr();
     html_filenames.forEach(el=>parse(el));
+});
+
+//void f(string)
+const toCompleteHtml = (html_string)=>{
+    let new_string = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><head><link href="../../src/css/general.css" rel="stylesheet" /></head>'
+    new_string+=html_string;
+    new_string+='</body></html>';
+    return new_string;
+}
+
+//void f(string,string)
+const write = (async (path,new_string)=>{
+        fs.writeFile(html_to_dir+path,new_string,(e)=>{
+            if(e)
+                console.error(e)
+        })
 });
 
 //void f(string)
@@ -31,12 +50,10 @@ const parse = (async (path)=>{
     //this has helped: X paragraphs
     return await fs.readFile(html_from_dir+path,(err,html_string)=>{
         if(err)throw err;
-        let new_string = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><head><link href="../../src/css/general.css" rel="stylesheet" /></head>'
-        new_string+='</body></html>';
-        fs.writeFile(html_to_dir+path,new_string,(e)=>{
-            if(e)
-                console.error(e)
-        })
+
+        let new_string = toCompleteHtml(html_string);
+        write(path,new_string);
+
     });
 })
 
