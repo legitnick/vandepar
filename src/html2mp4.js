32980a5819610a093daf75e1f6e9f9671924789c
Paul <yaveirick@gmail.com>
2023-02-06 19:37:34 +0200

    feat: dynamic path in html2mp4.js
    for universality

--- src/html2mp4.js
+++ src/html2mp4.js
@@ -2,26 +2,34 @@
 
 const puppeteer = require('puppeteer');
 const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
+const process = require('process');
 
-const goto_dir = "file:///home/pawlo/Documents/code/nodejs/vandepar/bin/scraped_html/";
+const curr_dir = process.cwd();
+const goto_dir = "file://" + curr_dir + "/bin/scraped_html/";//this variable needs full path to work in puppeteers Chromium
+console.log(goto_dir);//rm this
 const video_dir = "./bin/video/";
 
 const configForDynamic = {
-        followNewTab: true,
-        fps: 60,
-        videoFrame: {
-            width: 1920,
-            height: 1080,
-        },
-        videoCrf: 20,
-        videoCodec: 'libx264',
-        videoPreset: 'ultrafast',
-        videoBitrate: 12000,
-        autopad: {
-            color: 'black' | '#35A5FF',
-        },
-        aspectRatio: '16:9',
-    };
+    followNewTab: true,
+    fps: 60,
+    videoFrame: {
+        width: 1920,
+        height: 1080,
+    },
+    videoCrf: 20,
+    videoCodec: 'libx264',
+    videoPreset: 'ultrafast',
+    videoBitrate: 12000,
+    autopad: {
+        color: 'black' | '#35A5FF',
+    },
+    aspectRatio: '16:9',
+};
+
+//void f(Page)
+const look = (async (page)=>{
+    await setTimeout(scroll, 100 ,page);//(functionRef, ms, param1)
+}); 
 
 //void f(Page)
 const scroll = (async (page) =>{
@@ -42,7 +50,7 @@ const scroll = (async (page) =>{
     });
 });
 
-//void f(void)
+//void f(void) TODO: replace param void -> html_number(int)
 const transform = (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -54,12 +62,13 @@ const transform = (async () => {
         hasTouch: true
     });
 
-    const recorder = new PuppeteerScreenRecorder(page,Config);
+    const recorder = new PuppeteerScreenRecorder(page,configForDynamic);
 
-    await recorder.start(video_dir + replace.mp4);
-    await page.goto(goto_dir + 16657603.html, {waitUntil: 'networkidle0'});
+    await recorder.start(video_dir + "replace.mp4").catch((err)=>reject(err));//replace replace with html_number
+    await page.goto("file:///home/pauro/Documents/code/wrk/vandepar/vandepar/bin/scraped_html/16657603.html"
+        /*goto_dir + "16657603.html"*/, {waitUntil: 'networkidle0'});
 
-    await scroll(page);
+    await look(page);
 
     await recorder.stop();
     await browser.close();
