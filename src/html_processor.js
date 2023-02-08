25d42165243dba6d7d2f9bc5c2181dc6298122f3
Paul <yaveirick@gmail.com>
2023-02-08 09:25:00 +0200

    feat: removed half of html_processor functions to just make it work with CSS

--- src/html_processor.js
+++ src/html_processor.js
@@ -19,14 +19,14 @@ const html_from_dir = "./bin/scraped_html/";
 const html_to_dir = "./bin/processed_html/";
 
 //string[] f(void)
-const getHtmlArr = (async ()=>{
+const getHTMLArr = (async ()=>{
     return fs.readdirSync(html_from_dir);
 });
 //this has no business being async function, does it?
 
 //void f(void)
 const parseAll = (async ()=>{
-    const html_filenames = await getHtmlArr();
+    const html_filenames = await getHTMLArr();
     //console.log(html_filenames);
     html_filenames.forEach(async (el)=>{
         await parse(el);
@@ -34,7 +34,13 @@ const parseAll = (async ()=>{
 });
 
 //void f(string)
-const toCompleteHtml = (html_string)=>{
+const writeCompleteHTML = (path,html_string)=>{
+    let complete_html = toCompleteHTML(html_string);
+    write(path,complete_html);
+}
+
+//string f(string)
+const toCompleteHTML = (html_string)=>{
     let new_string = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><head><link href="../../src/css/general.css" rel="stylesheet" /></head>'
     new_string+=html_string;
     new_string+='</body></html>';
@@ -58,51 +64,11 @@ function reAddClass(class_string,html_string){
     return "<div class='"+class_string+"'>\n"+html_string+"\n</div>"
 }
 
-
-// string[] f(string)
-const splitHTML = (error,html_string)=>{
-    if(error)console.error(error);
-
-    const $ = cheerio.load(html_string);
-
-    const qs = $(".question-hyperlink").toArray().map(el=>$(el).html()).map(el=>reAddClass("question-hyperlink",el));
-    const posts = $(".s-prose").toArray().map(el=>$(el).html()).map(el=>reAddClass("s-prose",el));
-    const helpeds = $(".this-has-helped").toArray().map(el=>$(el).html()).map(el=>reAddClass("this-has-helped",el));
-    //too repetitive to not have a function 
-
-    let mixed_posts = [];
-    for(let i = 0;i < posts.length; i++){
-        mixed_posts.push(posts[i]);
-        if(i<helpeds.length)
-            mixed_posts.push(helpeds[i]);
-    }
-    const arr = qs.concat(mixed_posts); 
-
-    console.log(arr);
-    return arr;
-};
-
 //void f(string)
 const parse = (async (path)=>{
-    //for each post, create a different html file, as well as for
-    //this has helped: X paragraphs
-    let newdir = ""+parseInt(path);//get_int_string()?
+    fs.readFile(html_from_dir+path,"utf8",(err,data)=>writeCompleteHTML(path,data));
+    
 
-    /*if(fs.access(html_from_dir + newdir),fs.constants.F_OK,(err)=>{
-        console.log(`${file} ${err ? 'is not readable' : 'is readable'}`);
-    })*/
-    if(fs.existsSync(html_to_dir+newdir)){
-        console.log(html_to_dir+newdir+" Exists");
-        return new Promise((resolve)=>resolve());
-    }
-        //didn't even check for the correct thing
-
-    fs.mkdirSync(html_to_dir+newdir+"/");
-    return new Promise((resolve,reject)=>{
-        
-        let html; 
-            fs.readFile(html_from_dir+path,"utf8",splitHTML);
-})
 });
 parseAll();
 module.exports = parseAll;
