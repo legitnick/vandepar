76f2b433b6699f3f480f8e5ef656d260776f0f1c
Pavlo <ano3erone@gmail.com>
2023-02-10 21:43:16 +0200

    fix: try another approach
    still ProtocolError: Protocol error (Target.createTarget): Target closed.

--- src/html2mp4.js
+++ src/html2mp4.js
@@ -52,7 +52,19 @@ const scroll = (async (page) =>{
     return 1;
 });
 //void f(int,Browser)
-const transform = (async (html_number,page,recorder) => {
+const transform = (async (html_number,browser) => {
+
+
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1,
+        isLandscape: true,
+
+    });
+
+    const recorder = await new PuppeteerScreenRecorder(page,configForDynamic);
 
     await page.goto(mf.goto_dir+html_number+".html");
     await recorder.start(mf.video_dir + html_number +".mp4").catch((err)=>reject(err));//replace replace with html_number
@@ -87,20 +99,10 @@ const transformAll =(async ()=>{
 
     const browser = await puppeteer.launch().catch((e)=>console.error(e));
 
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 1,
-        isLandscape: true,
-
-    });
-
-    const recorder = await new PuppeteerScreenRecorder(page,configForDynamic);
     const html_arr = fs.readdirSync(mf.html_to_dir).filter(isHtmlUnused);
     const html_num_arr = html_arr.map(el=>parseInt(el));
 
-    await wrap(html_num_arr,page,recorder).then(async()=>browser.close());
+    await wrap(html_num_arr,browser).then(async()=>browser.close());
     /*
     await html_num_arr.map((async(el)=>{
         el = await transform(el,page,recorder);
