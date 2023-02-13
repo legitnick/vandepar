ce0161403af74a598419e2b9bcd6aed45360fdcf
Pavlo <ano3erone@gmail.com>
2023-02-13 10:13:34 +0200

    feat: working video conversion

--- src/html2mp4.js
+++ src/html2mp4.js
@@ -8,14 +8,14 @@ const mf = require("./myFiles.js");
 
 
 const configForDynamic = {
-    fps: 6/*0*/,
+    fps: 60,
     videoFrame: {
         width: 1920,
         height: 1080,
     },
     videoCodec: 'libx264',
     videoPreset: 'ultrafast',
-    videoBitrate: 1000,
+    videoBitrate: 4000,
     autopad: {
         color: 'black' | '#35A5FF',
     },
@@ -24,104 +24,91 @@ const configForDynamic = {
 
 //void f(Page)
 async function look(page){
-    return await scroll(page)
-        .catch((e)=>{
-            console.error(e);
-            reject(e);;
-        })
+    await new Promise(resolve=>setTimeout(resolve,100));
+
+    return await scroll(page);//(functionRef, ms, param1)
+
 };
 
 //void f(Page)
-const scroll = (async (page) =>{
-    await page.evaluate(async ()=>{
-        await new Promise((resolve)=>{
+async function scroll (page) {
+    return await page.evaluate(async ()=>{
+        return await new Promise((resolve)=>{
             let current_scrolled = 0;
-            let dist = 1;//px
+            let dist = 1;//scroll, px
             var timer = setInterval(()=>{
                 window.scrollBy(0,dist);
                 current_scrolled+=dist;
-                console.log(curren_scrolled+","+(document.body.scrollHeight - window.innerHeight));
+
                 if(current_scrolled > document.body.scrollHeight - window.innerHeight){
                     clearInterval(timer);
-                    resolve();
+                    setTimeout(resolve,100);
                 }
 
-            },20);
+            },20)//wait, ms
+        }).catch((e)=>{
+            console.error(e);
+            reject(e);;
+            });
         });
-    })
-    return 1;
-});
-//void f(int,Browser)
+    };
+
+//void f(int)
 const transform = (async (html_number) => {
+        const browser = await puppeteer.launch().catch((e)=>console.error(e));
+        const page = await browser.newPage().catch((e)=>console.error(e));
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+            isLandscape: true,
+        });
+
+        const recorder = new PuppeteerScreenRecorder(page,configForDynamic);
+
+        await recorder.start(mf.video_dir + html_number +".mp4").catch((err)=>reject(err));//replace replace with html_number
 
 
-    const browser = await puppeteer.launch().catch((e)=>console.error(e));
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 1,
-        isLandscape: true,
+        await page.goto(mf.goto_dir + html_number+ ".html", {waitUntil: 'networkidle0'}).catch((e)=>console.error(e));
+        await look(page);
+        await recorder.stop();
+        browser.close();
+        console.log("recorded");
 
+        /*return await look(page).then((res,rej)=>{
+            return recorder.stop();
+        }).then((res)=>browser.close()).catch((e)=>console.error(e));*/
     });
-
-    const recorder = await new PuppeteerScreenRecorder(page,configForDynamic);
-
-    await page.goto(mf.goto_dir+html_number+".html").catch(page.goto(mf.goto_dir+"u_"+html_number+".html"));
-    await recorder.start(mf.video_dir + html_number +".mp4").catch((err)=>reject(err));//replace replace with html_number
-
-    console.log("I've been there");
-
-    await scroll(page);
-    await recorder.stop();
-    console.log("recorded");
-    //await page.goto(mf.goto_dir + html_number+ ".html", {waitUntil: 'networkidle0'}).catch((e)=>console.error(e));
-    browser.close();//think I may not await for it
-    return 1;
-});
-//await everything
+    //await everything
 
 //void f(string);
 const renameToUsed = ((html_string) => {
-    mf.move(mf.html_to_dir+html_string,mf.html_to_dir+"u_"+html_string);
+    fs.renameSync(mf.html_to_dir+html_string,mf.html_to_dir+"u_"+html_string);
 });
 
 //filter html_arr with a function isHTMLUsed
 //bool f(string)
-function isHtmlUnused(html_string){
+const isHtmlUnused = (html_string)=>{
     return html_string.charAt(0)!=='u';//using filenames, dk what else to do here, except from some real DB
 }
 
-
-//look -> promise -> look, until i < len
- async function wrap(html_arr,page,recorder){
-  if(!html_arr.length) return 1;
-  transform(html_arr.slice(-1),page,recorder).then(()=>{html_arr.pop();wrap(html_arr)})
- }
-
 //void f(void)
-const transformAll =(async ()=>{
+    const transformAll = ()=>{
+        const html_arr = fs.readdirSync(mf.html_to_dir).filter(isHtmlUnused);
+        console.log(html_arr);
+        const html_num_arr = html_arr.map(el=>parseInt(el));
+
+        html_num_arr.forEach((el,i)=>{
+            if(i<4)
+                transform(el);
+        });
+        html_arr.forEach((el,i)=>{
+            if(i<4)
+                renameToUsed(el);
+        });
 
 
-    const html_arr = await fs.readdirSync(mf.html_to_dir).filter(isHtmlUnused);
-    const html_num_arr = await html_arr.map(el=>parseInt(el));
+    }
+    transformAll();
 
-    const promise_arr = [];
-    const this_time_arr = await html_num_arr.slice(0,4);
-    await this_time_arr.forEach((el)=>{
-        promise_arr.push(transform(el));
-    });
-    /*
-    await html_num_arr.map((async(el)=>{
-        el = await transform(el,page,recorder);
-    }));
-*/
-    Promise.all(promise_arr).then(html_arr.slice(0,4).forEach(renameToUsed)).then(setTimeout(transformAll,1000));
-
-    return 1;
-    /*return await look(page).then((res,rej)=>{
-            return recorder.stop();
-        }).then((res)=>browser.close()).catch((e)=>console.error(e));*/
-});
-
-module.exports = transformAll;
+    module.exports = transform;
