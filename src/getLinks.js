d46466b57926b1ff101f8ef7a0a749edb1c5f043
Pavlo <ano3erone@gmail.com>
2023-02-16 16:13:43 +0200

    feat: writing links into JSON to use later elsewhere

--- src/getLinks.js
+++ src/getLinks.js
@@ -1,4 +1,5 @@
 const stackexchange = require ("stackexchange");
+const fs = require ("fs");
 
 var options = { version: 2.2 };
 var context = new stackexchange(options);
@@ -7,19 +8,26 @@ var filter = {
     key: 'pwDNoBGQPWPm*rgcdmFBkw((',
     min: 20,
     pagesize: 50,
-    sort: 'activity',
+    sort: 'votes',
     order: 'asc'
 };
 
 // Get all the questions (http://api.stackexchange.com/docs/questions)
-context.questions.questions(filter, function(err, results){
-    if (err) throw err;
+async function getLinks(){
 
-    results.items =    results.items.map(el=>el.link);
-    console.log(results.items);
-});
+    context.questions.questions(filter, function(err, results){
+        if (err) throw err;
+
+        results.items =    results.items.map(el=>el.link);
+        console.log(results.items);
+        const data = {}
+        data.arr = results.items;
+        fs.writeFile("links.json",JSON.stringify(data),(err)=>console.error(err));
 
 
+    });
+}
+getLinks()
 /*
  * // Get results for a different website within the stackexchange network
 filter.site = 'softwareengineering';
