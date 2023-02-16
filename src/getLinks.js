053f9e74e5a3461fa577cae575274905a3ba286b
Pavlo <ano3erone@gmail.com>
2023-02-16 15:55:47 +0200

    feat: get a lot of links with at least 10 likes

--- src/getLinks.js
+++ src/getLinks.js
@@ -4,21 +4,24 @@ var options = { version: 2.2 };
 var context = new stackexchange(options);
 
 var filter = {
-  key: 'pwDNoBGQPWPm*rgcdmFBkw((',
-  pagesize: 50,
-  sort: 'activity',
-  order: 'asc'
+    key: 'pwDNoBGQPWPm*rgcdmFBkw((',
+    min: 20,
+    pagesize: 50,
+    sort: 'activity',
+    order: 'asc'
 };
 
 // Get all the questions (http://api.stackexchange.com/docs/questions)
 context.questions.questions(filter, function(err, results){
-  if (err) throw err;
+    if (err) throw err;
 
-  console.log(results.items);
-  console.log(results.has_more);
+    results.items =    results.items.map(el=>el.link);
+    console.log(results.items);
 });
 
-// Get results for a different website within the stackexchange network
+
+/*
+ * // Get results for a different website within the stackexchange network
 filter.site = 'softwareengineering';
 context.questions.questions(filter, function(err, results){
   if (err) throw err;
@@ -34,3 +37,4 @@ context.users.users(filter, function(err, results){
   console.log(results.items);
   console.log(results.has_more);
 });
+*/
