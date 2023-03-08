a198e08cb086edfd53578f95c5863241e4d99f88
Pavlo <ano3erone@gmail.com>
2023-03-08 11:16:33 +0200

    feat: first try to upload video
    there is a puppeteer error 'Channel name not found'

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
-
+import upload from "./src/upload.js";
 import transformAll from "./src/html2mp4.js";
 import scrape from "./src/scrape.js";
 import * as mf from "./src/myFiles.js";
 import edit from "./src/video_edit.js";
+
 async function main () {
-  await Promise.all([scrape (),
-    transformAll().catch(console.error),
-    edit()]);
+  await Promise.all([//scrape (),
+    //transformAll().catch(console.error),
+    //edit(),
+    upload()
+  ]);
 
   main();
 }
--- src/upload.js
+++ src/upload.js
@@ -1,5 +1,8 @@
 import { upload } from 'youtube-videos-uploader' //Typescript
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+await dotenv.config()
 
+console.log( process.env.GOOGLE_EMAIL+"\n" + process.env.GOOGLE_PASSWORD);
 // recoveryemail is optional, only required to bypass login with recovery email if prompted for confirmation
 const credentials = { email: process.env.GOOGLE_EMAIL, pass: process.env.GOOGLE_PASSWORD  }
 
@@ -12,8 +15,10 @@ const onVideoUploadSuccess = (videoUrl) => {
 // Extra options like tags, thumbnail, language, playlist etc
 const video2 = { path: './bin/video_complete/1125968.mp4', title: 'title 2', description: 'description 2', thumbnail:'thumbnail.png', language: 'english', tags: ['video', 'github'], playlist: 'playlist name', channelName: 'Channel Name', onSuccess:onVideoUploadSuccess, skipProcessingWait: true, onProgress: (progress) => { console.log('progress', progress) }, uploadAsDraft: false, isAgeRestriction: false, isNotForKid: false,  }
 
-
+console.log("enter uploadjs");
 
 // OR
 // This package uses Puppeteer, you can also pass Puppeteer launch configuration
-upload (credentials, [ video2], {headless:false}).then(console.error);
+const upload_YT = ()=>{upload (credentials, [ video2], {headless:false}).then(console.error)};
+upload_YT();
+export default upload_YT;
